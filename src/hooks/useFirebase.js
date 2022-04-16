a83c16294d1bf4a64fdcbdc7132a3686c173946f
sayed48 <masayedmas48@gmail.com>
2022-04-16 18:05:13 +0600

    signin and signout method added

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react"
+import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
-    useEffect(()=>{
-
-    }, []);
     const signInWithGoogle = () =>{
-        console.log("sign in")
-    } 
-    return [user, setUser];
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user);
+            console.log(user);
+        })
+    }
+
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(() =>{})
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUser(user);
+        })
+    }, [])
+
+    return {
+        user, 
+        handleSignOut,
+        signInWithGoogle
+    }
 }
 export default useFirebase;
