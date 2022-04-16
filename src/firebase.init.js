1e5a96286bd0c8b8f488fea848c2bdffb64c903a
sayed48 <masayedmas48@gmail.com>
2022-04-16 13:36:11 +0600

    herder and ligin components added

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5XD1i_MDfeZb8jJhuszYiau5Xco-JAfI",
+  authDomain: "router-firebase-integrat-1f0c4.firebaseapp.com",
+  projectId: "router-firebase-integrat-1f0c4",
+  storageBucket: "router-firebase-integrat-1f0c4.appspot.com",
+  messagingSenderId: "53212831599",
+  appId: "1:53212831599:web:27ff3527180a3f650bd29a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
